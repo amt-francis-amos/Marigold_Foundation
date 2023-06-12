d52307f0107e21c169edf5f1b31888c21ebe7353
Francisamos123 <sugartone45@yahoo.com>
2023-06-12 01:52:32 +0000

    commit to main

---
+++ script.js
@@ -0,0 +1,17 @@
+// ---- MENU BAR ------
+
+
+let show = document.getElementById("nav-links")
+
+  
+function showMenu(){
+   show.style.right = "0";
+   console.log('clicked')
+}
+
+function closeMenu(){
+   show.style.right = "-200px";
+   console.log('clicked me')
+}
+
+
